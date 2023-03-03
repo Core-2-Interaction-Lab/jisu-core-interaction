21125d8418f7ee9d57ccf5ba20fc35192ee0a4b6
kangj064 <113397395+kangj064@users.noreply.github.com>
2023-03-03 12:42:40 -0500

    yy

---
+++ project-2/script.js
@@ -0,0 +1,66 @@
+const collection = [
+
+    {
+        'title' : 'In That Time, I realized it',
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2019,
+        'category' : 'Public',
+        
+    },
+    { 
+        'title' : 'Politic changed brain of person'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2018,
+        'category' : 'Eassy',
+    },
+    { 
+        'title' : 'Emotional Democaracy'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2017,
+        'category' : 'Public',
+
+    },
+    { 
+        'title' : 'Shoppin is more important than voting'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2017,
+        'category' : 'Criticism',
+
+    },
+    { 
+        'title' : 'Shoppin is more important than voting'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2017,
+        'category' : 'Criticism',
+
+    },
+    { 
+        'title' : 'the country of babel'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2017,
+        'category' : 'Criticism',
+
+    },
+    { 
+        'title' : 'Korean Politic'
+        'url' : 'http://www.yes24.com/Product/Goods/93975042',
+        'yearPublished' : 2017,
+        'category' : 'Criticism',
+
+    },
+    { 
+        'title' : 'Outside and Inside of the Media'
+        'url' : '',
+        'yearPublished' : 2002,
+        'category' : 'Media',
+
+    },
+    { 
+        'title' : 'Shaman '
+        'url' : '',
+        'yearPublished' : ,
+        'category' : '',
+
+    },
+];
+];
