8d0ecab5175480b570d24b04dd06ca8aba2a1b12
kangj064 <113397395+kangj064@users.noreply.github.com>
2023-05-11 07:19:37 -0400

    ZX

--- project-3/dataviz demo/assets/mycharts.js
+++ project-3/dataviz demo/assets/mycharts.js
@@ -48,16 +48,8 @@ function createPieChart(labels, data) {
             datasets: [{
                 data: data,
                 backgroundColor: [
-                    '#ff6384',
-                    '#36a2eb',
-                    '#cc65fe',
-                    '#ffce56',
-                    '#b7dbab',
-                    '#a5c3d9',
-                    '#ff7f50',
-                    '#cd5c5c',
-                    '#f08080',
-                    '#ffa07a'
+                    '#F94143',
+            
                 ]
             }]
         },
@@ -69,3 +61,85 @@ function createPieChart(labels, data) {
         }
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch('assets/allbites.json')
+    .then(response => response.json())
+    .then(data => {
+        const genderCounts = data.reduce((counts, item) => {
+            counts[item.gender] = (counts[item.gender] || 0) + 1;
+            return counts;
+        }, {});
+
+        const genderNames = Object.keys(genderCounts);
+        const genderCountsArray = Object.values(genderCounts);
+
+        // Call the function to create the chart
+        createPieChart(genderNames, genderCountsArray);
+    });
+
+
+// make the chart
+function createPieChart(labels, data) {
+    const canvas = document.getElementById('genderChart');
+    const ctx = canvas.getContext('2d');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#ff6384',
+            
+                ]
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: 'right'
+            }
+        }
+    });
+}
+
+
+
+const ctx = document.getElementById('topChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Pitbull', 'shih tuz', 'chihuahua'],
+        datasets: [{
+            label: '# of Votes',
+            data: [194, 34, 17],
+            backgroundColor: [
+                '#F94143'
+            ],
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
